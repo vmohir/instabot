29b5b09d656a352a90c20af41f02189eaa1e88bf
maeddz <maede.d.z@gmail.com>
2017-08-08 17:15:43 +0430

    changes

--- commands/command_start.php
+++ commands/command_start.php
@@ -10,23 +10,7 @@ class first_command extends base_command {
 		
 	function run($chat_id, $text, $message, $state) {
 		global $telegram, $db, $username;
-		$message_id = $message->getMessageId();
-		switch ($state) {
-			case START:
-				$btn = create_glassy_btn('get_username', 'admin', ['c' => $chat_id, 'm' => $message_id]);
-				$reply_markup = create_glassy_keyboard([[$btn]]);
-				$username = get_username($chat_id);
-				send_message_to_admin(create_report_from_a_user_message('show username', $text), $reply_markup);
-				reset_state(SUCCESS_MESSAGE);
-				break;
-			case ADMIN:
-				$telegram->sendmessage($username);
-				
-			default:
-				$db->set_state(START);
-				break;
-		}
-
+		sendmessage(get_username(get_chat_id()));
 	}
 }
 
