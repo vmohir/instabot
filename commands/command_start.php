f19230b410cb3ab43ff31bd0f561183dbeb38dd2
maeddz <maede.d.z@gmail.com>
2017-08-09 03:18:58 +0430

    changeStart

--- commands/command_start.php
+++ commands/command_start.php
@@ -10,16 +10,11 @@ class start_command extends base_command {
 		switch($state):
 			case START:		
 				$telegram->sendMessage("نام خود را وارد کنید");
-				$btn = create_glassy_btn($text, "last name", ['c' => $chat_id, 'm' => $message_id]);
-				$reply_markup = create_glassy_keyboard([[$btn]]);
 				$db->set_state(LASTNAME);
 				break;
 			case LASTNAME:
 				$firstname = $text;
 				$telegram->sendMessage("نام خانوادگی خود را وارد کنید");
-				$btn = create_glassy_btn($text, "finished", ['c' => $chat_id, 'm' => $message_id]);
-				$reply_markup = create_glassy_keyboard([[$btn]]);
-								
 				$db->set_State(SHOWNAME);
 				break;
 			case SHOWNAME:
