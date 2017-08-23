f9a4413af02f13ca4c994f699a39e3fb6273dc78
maeddz <maede.d.z@gmail.com>
2017-08-23 22:14:00 +0430

    changes

--- commands/command_start.php
+++ commands/command_start.php
@@ -22,29 +22,10 @@ class start_command extends base_command {
 				log_debug($text, 110179059);
 				break;
 			default:
-				if($check==0){
-					sendMessage("نام خود را وارد کنید");
-					$db->set_state(START);	
-					$check=1;
-					break;
-				}
-				if($check==1){
-					$firstname = $text;
-					sendMessage("نام خانوادگی خود را وارد کنید");
-					$db->set_state(LASTNAME);
-					log_debug($text, 110179059);
-					$check=2;
-					break;
-				}
-				if($check==2){
-					log_debug($text, 110179059);
-					$lastname = $text;
-					sendMessage($firstname . $lastname);
-					reset_state();
-					log_debug($text, 110179059);
-					$check=3;
-					break;
-				}
+				sendMessage("نام خود را وارد کنید");
+				$db->set_state(START);	
+				break;
+				
 		}
 		
 	}
